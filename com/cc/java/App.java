b4b10bdaa4ad275893aa5a619b0500a675e2dd73
Firas <firas.allzaydy@gmail.com>
2024-01-24 11:45:16 +0100

    02/klassen_objekt / wertzuweisung + Ausgabe

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,35 @@ public class App {
     
     public static void main(String[] args) {
 
+        // objekt entsteht ...
+        Cat cat = new Cat(); // Instanzierung
+        System.out.println(cat);
+
+        // wertzuweisung
+        cat.name = "Alonzo";
+        cat.furcolor = "grey";
+        cat.age = 35;
+
+        // Ausgabe
+        System.out.println(cat.name);
+        System.out.println(cat.furcolor);
+        System.out.println(cat.age);
+
+        System.out.println(("-------------"));
+
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+
+        // wertzuweisung
+        cat.name = "Grizabella";
+        cat.furcolor = "white";
+        cat.age = 29;
+        
+        // Ausgabe
+        System.out.println(cat.name);
+        System.out.println(cat.furcolor);
+        System.out.println(cat.age);
+        
 
     }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,23 @@
+package com.cc.java;
+
+public class Cat {
+
+   String name;     // Primitives
+   String furcolor; // Primitives
+   int age;         // Primitives
+
+
+
+   
+public void tellYourName(){
+System.out.println(this.name);
+}
+
+
+    public void tellYouraddress(){
+        System.out.println(this);
+    }
+
+
+
+}
